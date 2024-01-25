6a4fda3803681dc0f5b5b4b9182ecf9f770991d9
Daniel Mauderer <daniel.mauderer@outlook.com>
2024-01-25 21:46:29 +0100

    fix checksum

--- src/service/gpio.rs
+++ src/service/gpio.rs
@@ -185,9 +185,13 @@ fn read_data(array: &mut [u8; 5], pin: &IoPin) -> Result<(), Box<dyn std::error:
     let temp = ((array[0] as u16) << 8) | array[1] as u16;
     let hum = ((array[2] as u16) << 8) | array[3] as u16;
     error!("Temp: {} Hum: {}", temp, hum);
-    let sum = (array[0] + array[1] + array[2] + array[3]);
-    let check_sum = sum & 0xFF;
-    if array[4] != check_sum {
+    if array[4]
+        != (array[0]
+            .wrapping_add(array[1])
+            .wrapping_add(array[2])
+            .wrapping_add(array[3])
+            & 0xFF)
+    {
         return Err(Box::from("Checksum"));
     }
     Ok(())
