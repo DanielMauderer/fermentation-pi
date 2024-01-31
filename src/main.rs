526898fa5c827812e5dfef413f222303a6acc9ca
Daniel Mauderer <daniel.mauderer@outlook.com>
2024-01-31 01:15:43 +0100

    ww

--- src/main.rs
+++ src/main.rs
@@ -33,6 +33,8 @@ fn rocket() -> _ {
     let _ = thread::spawn(|| async {
         warn!("w1");
         let error = basic_runners::manage_climate::entry_loop().await;
+        warn!("ww1");
+
         let e = error.err().unwrap();
         error!("manage_climate shut down with error: {}", e);
     });
