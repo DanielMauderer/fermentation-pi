88bf79bc97daa5113b3969eede4403ee1fc77e5b
Daniel Mauderer <daniel.mauderer@outlook.com>
2024-03-27 15:50:39 +0100

    pid values

--- src/basic_runners/manage_climate.rs
+++ src/basic_runners/manage_climate.rs
@@ -10,6 +10,7 @@ use crate::service::{
 
 const TEMP_DUTY_CYCLE: f32 = 1.0;
 const HUM_DUTY_CYCLE: f32 = 10.0;
+const PID_LIMIT: f32 = 100.0;
 
 pub fn entry_loop_hum() {
     let project = match get_active_project() {
@@ -19,8 +20,11 @@ pub fn entry_loop_hum() {
             return;
         }
     };
-    let mut hum_pid: Pid<f32> = Pid::new(project.settings.hum, 100.0);
-    hum_pid.p(10.0, 100.0).i(4.5, 100.0).d(0.25, 100.0);
+    let mut hum_pid: Pid<f32> = Pid::new(project.settings.hum, PID_LIMIT);
+    hum_pid
+        .p(10.0, PID_LIMIT)
+        .i(4.5, PID_LIMIT)
+        .d(0.25, PID_LIMIT);
 
     let mut sensor_data: SensorData = match get_sensor_data() {
         Ok(sensor_data) => sensor_data,
@@ -31,28 +35,29 @@ pub fn entry_loop_hum() {
     };
 
     loop {
-        let hum_on_time = hum_pid.next_control_output(sensor_data.hum).output / 100.0;
+        let hum_on_percentage = hum_pid.next_control_output(sensor_data.hum).output / PID_LIMIT;
+        let hum_on_time = hum_on_percentage * HUM_DUTY_CYCLE;
+        let hum_off_time = HUM_DUTY_CYCLE - hum_on_time;
         warn!("hum_on_time: {}", hum_on_time);
         task::spawn(async move {
-            if hum_on_time > 0.0 {
+            if hum_on_percentage > 0.0 {
                 match turn_on_humidifier() {
                     Ok(_) => {}
                     Err(e) => {
                         error!("Error: {}", e);
                     }
                 };
-                task::sleep(Duration::from_secs_f32(HUM_DUTY_CYCLE * hum_on_time)).await;
-                match turn_off_humidifier() {
-                    Ok(_) => {}
-                    Err(e) => {
-                        error!("Error: {}", e);
-                    }
-                };
+                task::sleep(Duration::from_secs_f32(hum_on_time)).await;
+                if hum_on_percentage < 0.995 {
+                    match turn_off_humidifier() {
+                        Ok(_) => {}
+                        Err(e) => {
+                            error!("Error: {}", e);
+                        }
+                    };
+                }
             }
-            task::sleep(Duration::from_secs_f32(
-                HUM_DUTY_CYCLE - (HUM_DUTY_CYCLE * hum_on_time),
-            ))
-            .await;
+            task::sleep(Duration::from_secs_f32(hum_off_time)).await;
         });
 
         thread::sleep(std::time::Duration::from_secs_f32(HUM_DUTY_CYCLE));
@@ -74,9 +79,12 @@ pub fn entry_loop_temp() {
             return;
         }
     };
-    let mut temp_pid: Pid<f32> = Pid::new(project.settings.temp, 100.0);
+    let mut temp_pid: Pid<f32> = Pid::new(project.settings.temp, PID_LIMIT);
     info!("temp_pid: {:?}", project.settings.temp);
-    temp_pid.p(2.5, 100.0).i(0.2, 100.0).d(2.5, 100.0);
+    temp_pid
+        .p(2.5, PID_LIMIT)
+        .i(0.2, PID_LIMIT)
+        .d(2.5, PID_LIMIT);
 
     let mut sensor_data: SensorData = match get_sensor_data() {
         Ok(sensor_data) => sensor_data,
@@ -92,21 +100,23 @@ pub fn entry_loop_temp() {
         }
     };
     loop {
-        let temp_on_time = temp_pid.next_control_output(sensor_data.temp).output / 100.0;
+        let temp_on_percentage = temp_pid.next_control_output(sensor_data.temp).output / PID_LIMIT;
+        let temp_on_time = temp_on_percentage * TEMP_DUTY_CYCLE;
+        let temp_off_time = TEMP_DUTY_CYCLE - temp_on_time;
         warn!(
             "target_temp: {} current_temp: {} temp_on_time: {}",
             project.settings.temp, sensor_data.temp, temp_on_time
         );
         task::spawn(async move {
-            if temp_on_time > 0.0 {
+            if temp_on_percentage > 0.0 {
                 match turn_on_heating() {
                     Ok(_) => {}
                     Err(e) => {
                         error!("Error: {}", e);
                     }
                 }
-                task::sleep(Duration::from_secs_f32(TEMP_DUTY_CYCLE * temp_on_time)).await;
-                if temp_on_time < 0.995 {
+                task::sleep(Duration::from_secs_f32(temp_on_time)).await;
+                if temp_on_percentage < 0.995 {
                     match turn_off_heating() {
                         Ok(_) => {}
                         Err(e) => {
@@ -115,10 +125,7 @@ pub fn entry_loop_temp() {
                     };
                 }
             }
-            task::sleep(Duration::from_secs_f32(
-                TEMP_DUTY_CYCLE - (TEMP_DUTY_CYCLE * temp_on_time),
-            ))
-            .await;
+            task::sleep(Duration::from_secs_f32(temp_off_time)).await;
         });
         thread::sleep(std::time::Duration::from_secs_f32(TEMP_DUTY_CYCLE));
         sensor_data = match get_sensor_data() {
